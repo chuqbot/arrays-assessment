a69e124c1a51e397db99311deca5ff355999c553
Jeremy C <jchuq27@gmail.com>
2023-11-14 21:51:56 -0800

    commit

---
+++ arrays.js
@@ -0,0 +1,18 @@
+for(let i = 1000; i > 0; i -= 2) {
+    console.log(i);
+    }
+    
+    for(let i = 0; i < 10000; i++) {
+        if (i = 2500) {
+        } alert("quarter of the way there!");
+        if (i = 5000) {
+        } alert("hallfway there!");
+        if (i = 10000) {
+        alert("the loop is done!");
+        }
+    }
+    let total = 0;
+    const favoriteTVShows = ["Better Call Saul", "Breaking Bad", "The Boys", "Barry", "Regular Show"];
+    for (let i = 0;  i < favoriteTVShows.length; i++) {
+        console.log("My #" +(i+1)+ "Favorite tv show is" +favoriteTVShows[i]);
+    }
